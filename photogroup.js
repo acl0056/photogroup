1fce2448924002e9e7681387c69002b63645bdc7
Adam Lockhart <adamlockhart@me.com>
2013-04-08 22:58:07 -0500

    Update photogroup.js
    Added photo group navigation.

--- photogroup.js
+++ photogroup.js
@@ -14,7 +14,7 @@ jQuery.fn.photoGroup = function(options) {
 		$parent = this.parent(),
 		parentId = this.attr("id"),
 		outerWidth = $parent.innerWidth() - parseInt($parent.css('padding-left')) - parseInt($parent.css('padding-right')),
-		w=0, h=0, row=0, col=0, tOuterWidth, tBorderSize, html, rows, columns;
+		w=0, h=0, row=0, col=0, tOuterWidth, tBorderSize,borderOffset, html, rows, columns, backHtml = [], imageHtml = [];
 		
 	if (!$.photoGroupScalars)
 		$.photoGroupScalars = {};
@@ -33,9 +33,11 @@ jQuery.fn.photoGroup = function(options) {
 	tOuterWidth = tBorderSize + thumbWidth + tBorderSize;
 	columns = Math.floor( outerWidth / tOuterWidth );
 	rows = Math.ceil( photoUrlsCount / columns );
+	borderOffset =  (tBorderSize - (tBorderSize % 2))/2;
 	
 	// Generate the html
 	html = ['<div style="width:',outerWidth,'px;height:',(rows*(tBorderSize+thumbHeight+tBorderSize)),'px;" >'];
+	
 	for (i=0;i<photoUrlsCount;i++){
 		var photoUrl = photoUrls[i], imageUrl, thumbUrl, x = col*thumbWidth, y = row*thumbHeight, imgId = 'pGrpImg'+$.photoGroupCount+parentId+i;
 		if (typeof photoUrl == "string")
@@ -46,14 +48,15 @@ jQuery.fn.photoGroup = function(options) {
 			imageUrl = photoUrl.image;
 			thumbUrl = photoUrl.thumb;
 		}
-		
-		html = html.concat([
-			'<div style="position:absolute;top:',y+tBorderSize,'px;left:',x,'px;display:inline;width:',
-					thumbWidth-tBorderSize,'px;height:',thumbHeight-tBorderSize,'px;" class=photoGroupBackground />',
+		backHtml = backHtml.concat([
+			'<div style="position:absolute;top:',y,'px;left:',x,'px;display:inline;width:',
+				thumbWidth-tBorderSize,'px;height:',thumbHeight-tBorderSize,'px;" class=photoGroupBackground />'
+		]);
+		imageHtml = imageHtml.concat([
 			'<img class=pGrpImg id=',imgId,' src="',photoUrl,'" style="position:absolute;top:0;left:0;display:none;"',
 			' onload="var $t=$(this);$t.css({display:&quot;&quot;});var w=parseInt($t.css(&quot;width&quot;)),h=parseInt($t.css(&quot;height&quot;));',
 			'var v,id=&quot;',imgId,'&quot;,s=$.photoGroupScalars[id]=(w>h?(',thumbWidth,'/w):(',thumbHeight,'/h));',
-			'v=$.photoGroupVectors[id]=[',x,'-(w-',thumbWidth,')/2, ',y,'-(h-',thumbHeight,')/2];',
+			'v=$.photoGroupVectors[id]=[',x+borderOffset,'-(w-',thumbWidth,')/2, ',y+borderOffset,'-(h-',thumbHeight,')/2];',
 			'$t.css({ translate:[v[0],v[1]], scale:s });" />',
 		]);
 		if (col == columns-1) {
@@ -63,6 +66,7 @@ jQuery.fn.photoGroup = function(options) {
 		else
 			col++;
 	}
+	html = html.concat(backHtml, imageHtml);
 	html.push("</div>");
 	html = html.join("");
 	this.html(html);
@@ -97,11 +101,17 @@ jQuery.fn.photoGroup = function(options) {
 			var $this = $(this),
 				thisId = $this.attr("id"),
 				$prev = $this.prev(),
+				$next = $this.next(),
+				parentOffset = $this.parent().offset(),
+				$window = $(window),
+				offsetTop = parentOffset.top - $window.scrollTop(),
+				offsetLeft = parentOffset.left - $window.scrollLeft(),
 				w = parseInt($this.css("width")),
 				h = parseInt($this.css("height")),
 				src = $this.attr("src"),
 				ww = window.innerWidth,
 				wh = window.innerHeight,
+				v = $.photoGroupVectors[thisId],
 				scale = 1, iw, ih, x, y;
 			
 			$this.off('mouseleave').off('mouseenter').off('click');
@@ -113,20 +123,59 @@ jQuery.fn.photoGroup = function(options) {
 			ih = parseInt(scale*h);
 			x = (ww-iw)/2 - (w-iw)/2;
 			y = (wh-ih)/2 - (h-ih)/2;
+			var style = " style='position:fixed;width:40px;height:30px;top:";
 			$("body").append(
-				"<div id=imgViewMask style='position:fixed;background-color:rgba(0,0,0,0.5);top:0;left:0;width:"+ww+"px;height:"+wh+"px;' />"
+				"<div id=imgViewMask style='position:fixed;background-color:rgba(0,0,0,0.5);top:0;left:0;width:"+ww+"px;height:"+wh+"px;' >"+
+					"<div id=photoGroupPrev class=photoGroupButton"+style+(wh/2-15)+"px;left:10px;' >&nbsp;&lt;&nbsp;</div>"+
+					"<div id=photoGroupNext class=photoGroupButton"+style+(wh/2-15)+"px;left:"+(ww-50)+"px;' >&nbsp;&gt;&nbsp;</div>"+
+				"</div>"
 			);
+				
 			var $mask = $("#imgViewMask");
 			$mask.append($this);
-			$this.css({ translate:[x,y],scale: scale });
+			if ($.photoGroupNavigation)
+				$this.css({ translate:[x,y],scale: scale });
+			else {
+				$this.css({ translate:[v[0]+offsetLeft,v[1]+offsetTop] });
+				$this.transition({ translate:[x,y],scale: scale });
+			}
+			$.photoGroupNavigation = true;
+			
 			$mask.click(function(){
+				$this.transition({ translate:[v[0]+offsetLeft,v[1]+offsetTop], scale: $.photoGroupScalars[thisId] }, "fast", function(){
+					$prev.after($this);
+					$this.css({"z-index":1});
+					$this.css({ translate:[v[0],v[1]] });
+					$this.on('mouseenter', $.pGrpImgMouseEnter).on('mouseleave', $.pGrpImgMouseleave).on('click', $.pGrpImgClick);
+					$mask.remove();
+					$.photoGroupNavigation = false;
+				});
+			});
+			
+			// PhotoGroup navigation handlers
+			$("#photoGroupPrev").click(function(e){
+				e.stopPropagation();
+				$this.css({ translate:[v[0],v[1]], scale: $.photoGroupScalars[thisId] });
 				$prev.after($this);
 				$this.css({"z-index":1});
-				var v = $.photoGroupVectors[thisId];
-				$this.css({ translate:[v[0],v[1]], scale: $.photoGroupScalars[thisId] });
 				$this.on('mouseenter', $.pGrpImgMouseEnter).on('mouseleave', $.pGrpImgMouseleave).on('click', $.pGrpImgClick);
-				$(this).remove();
+				$mask.remove();
+				$prev.click();
 			});
+			if (!$prev.hasClass("pGrpImg"))
+				$("#photoGroupPrev").hide();
+			
+			$("#photoGroupNext").click(function(e){
+				e.stopPropagation();
+				$this.css({ translate:[v[0],v[1]], scale: $.photoGroupScalars[thisId] });
+				$prev.after($this);
+				$this.css({"z-index":1});
+				$this.on('mouseenter', $.pGrpImgMouseEnter).on('mouseleave', $.pGrpImgMouseleave).on('click', $.pGrpImgClick);
+				$mask.remove();
+				$next.click();
+			});
+			if (!$next.hasClass("pGrpImg"))
+				$("#photoGroupNext").hide();
 		}
 	}
 	$(".pGrpImg").off('mouseenter').on('mouseenter', $.pGrpImgMouseEnter)
